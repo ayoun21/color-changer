0f6a3409277bc1927cecb9ba779dfd61fd898d2f
ayoun21 <ayoun0851@withs.org>
2016-04-14 14:35:25 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
+var colors = ["blue", "magenta", "green", "red"];
 
+function changeColor() {
+    var randomIndex = Math.floor((Math.random() * colors.length) + 0);
+
+    $("body").css("background-color", colors[randomIndex]);
+}
+
+function addColor() {
+    var newColor = $("#newColor").val();
+    colors.push(newColor);
+   $("body").css("background-color", newColor);
+}
+
+ $( document ).ready(function() {
+     $("#change").click(changeColor);
+     $("#add").click(addColor);
+ });
